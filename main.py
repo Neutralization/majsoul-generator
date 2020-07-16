f95305ab4b3d6ad01f0c4e6dfa07cbcf90c0f9b1
Neutralization <50076677+Neutralization@users.noreply.github.com>
2020-07-17 01:25:20 +0800

    add Yama

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 
 import re
-from tkinter import StringVar, Tk, ttk
 from functools import reduce
+from tkinter import StringVar, Tk, ttk
+
 from PIL import Image
 
 
-def makeImage():
-    test = textBox.get()
+def makeImage(text):
+    if not text:
+        return 0
     image_list = []
-    parts = test.split(' ')
+    parts = text.split(' ')
     for i in range(len(parts)):
         part = parts[i]
         results = re.findall(r'([0-9x]+[mpsz])', part)
@@ -20,7 +22,9 @@ def makeImage():
                         for result in results]))
             results = sorted(sorted(results), key=lambda x: x[-1])
         for result in results:
-            image_list += ['./ui/{}{}.png'.format(x, result[-1]) for x in result[:-1]]
+            image_list += [
+                './ui/{}{}.png'.format(x, result[-1]) for x in result[:-1]
+            ]
         image_list.append('./ui/0.png')
 
     print(image_list)
@@ -31,18 +35,55 @@ def makeImage():
     for img in imagefile:
         target.paste(img, (left, 0))
         left += img.size[0]
-    target.save('{}.png'.format(test.replace(' ', '_')), quality=100)
+    target.save('{}.png'.format(text.replace(' ', '_')), quality=100)
+
+
+def makeYamaImage(text):
+    if text:
+        image_list = []
+        results = re.findall(r'([0-9x]+[mpsz])', text)
+        for result in results:
+            image_list += [
+                './ui/{}{}.png'.format(x, result[-1]) for x in result[:-1]
+            ]
+        imagefile = [Image.open(x) for x in image_list
+                     ] + (5 - len(image_list)) * [Image.open('./ui/xz.png')]
+    else:
+        imagefile = 5 * [Image.open('./ui/xz.png')]
+    target = Image.new('RGBA', (5 * 80, 130))
+    left = 0
+    for img in imagefile:
+        target.paste(img, (left, 0))
+        left += img.size[0]
+    target.save('{}.png'.format(text if text else 'Yama'), quality=100)
+
+
+def make():
+    makeImage(textBox.get())
+    makeYamaImage(doraBox.get())
+    makeYamaImage(uraBox.get())
 
 
 window = Tk()
 window.title("和牌生成器")
 window.resizable(False, False)
 
+ttk.Label(window, text='手牌').grid(column=0, row=0, sticky='W')
 textBox = StringVar()
 textBox_entered = ttk.Entry(window, width=50, textvariable=textBox)
-textBox_entered.grid(column=0, row=0)
+textBox_entered.grid(column=0, row=1)
 
-Button = ttk.Button(window, text='生成', command=makeImage)
-Button.grid(column=1, row=0)
+ttk.Label(window, text='牌山-表').grid(column=0, row=2, sticky='W')
+doraBox = StringVar()
+doraBox_entered = ttk.Entry(window, width=50, textvariable=doraBox)
+doraBox_entered.grid(column=0, row=3)
+
+ttk.Label(window, text='牌山-里').grid(column=0, row=4, sticky='W')
+uraBox = StringVar()
+uraBox_entered = ttk.Entry(window, width=50, textvariable=uraBox)
+uraBox_entered.grid(column=0, row=5)
+
+Button = ttk.Button(window, text='生成', command=make)
+Button.grid(column=1, row=5)
 
 window.mainloop()
